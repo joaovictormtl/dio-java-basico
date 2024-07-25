f64d50921d51149c756bfbc0889108da794d11b6
johnstack <joaovictormtl@gmail.com>
2024-07-25 14:11:37 -0300

    Variávies e Métodos

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -1,5 +1,33 @@
 public class MinhaClasse {
     public static void main(String [] args){
-        System.out.print("Olá, Mundo!");
+        String nome = "João";
+        String sobrenome = "Melo Lima";
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+        System.out.println(nomeCompleto);
+
+        long cep = 51250370;
+        System.out.println(formatarCep(cep));
+
+        double mediaFinal = 5;
+        System.out.println(aprovado(mediaFinal));
+    }
+
+    public static String nomeCompleto(String nome, String sobrenome){
+        return "Resultado do Método: " + nome.concat(" ").concat(sobrenome);
+    }
+
+    public static String formatarCep(long cep){
+        String strCep = Long.toString(cep);
+        String pp = strCep.substring(0, 2).concat(".");
+        String sp = strCep.substring(2, 5).concat("-");
+        String tp = strCep.substring(5);
+        return pp + sp + tp;
+    }
+
+    public static boolean aprovado(double mediaFinal){
+        if(mediaFinal >= 6){
+            return true;
+        }
+        return false;
     }
 }
