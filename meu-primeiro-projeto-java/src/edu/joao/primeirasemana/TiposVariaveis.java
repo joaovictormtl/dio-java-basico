0b1306e7fbfd5dd5272eb7bf8a53d76dd2d39c13
johnstack <joaovictormtl@gmail.com>
2024-07-25 15:57:56 -0300

    Criação de variáveis e constantes

--- meu-primeiro-projeto-java/src/edu/joao/primeirasemana/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/edu/joao/primeirasemana/TiposVariaveis.java
@@ -2,6 +2,18 @@ package edu.joao.primeirasemana;
 
 public class TiposVariaveis {
     public static void main(String [] args){
+        // Variáveis
+        int idade;
+        idade = 22;
+
+        int anoFabricacao = 2001;
+        anoFabricacao++;
+
+        double salarioMinimo = 1500;
+        String nome = "João Victor";
         
+        // Constantes
+        final float PI = 3.1415F;
+        final int NUMERO_ESTADOS = 27;
     }
 }
