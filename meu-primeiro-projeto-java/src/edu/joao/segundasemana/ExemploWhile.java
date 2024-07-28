001f92b26dc786de8d3fffd80c46e30dbc7db336
johnstack <joaovictormtl@gmail.com>
2024-07-28 18:27:57 -0300

    feat:"add quebrando um laço while infinito conforme uma condição"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploWhile.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploWhile.java
@@ -2,6 +2,23 @@ package edu.joao.segundasemana;
 
 public class ExemploWhile {
     public static void main(String[] args) {
-        System.out.println("Tudo Certo!");
+        double mesada = 50.0;
+        while(true){
+            Double valorDoce = valorAleatorio();
+            if(valorDoce <= mesada){
+                mesada -= valorDoce;
+                System.out.println("Doce: R$ -"+valorDoce);
+                System.out.println("Mesada Atual: R$ "+mesada);
+                System.out.println();
+                continue;
+            }
+            System.out.println("Doce: R$ "+valorDoce);
+            System.out.println("Mesada Insuficiente: R$ "+mesada);
+            break;
+        }
+    }
+
+    public static double valorAleatorio(){
+        return Math.round(1 + Math.random() * 9);
     }
 }
