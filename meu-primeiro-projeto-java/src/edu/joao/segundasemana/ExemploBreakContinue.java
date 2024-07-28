b5278cefca61d945d8748108ad65d202d84b02f7
johnstack <joaovictormtl@gmail.com>
2024-07-28 18:06:24 -0300

    feat:"add pular uma interação com base numa condição"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploBreakContinue.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploBreakContinue.java
@@ -3,8 +3,12 @@ package edu.joao.segundasemana;
 public class ExemploBreakContinue {
     public static void main(String [] args){
         for(int numero = 1; numero <= 5; numero++){
-            if(numero == 3){
+            if(numero == 4){
                 break;
+            }
+
+            if(numero == 3){
+                continue;
             }
             System.out.println(numero);
         }
