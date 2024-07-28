01e10630bb0572f6b1533af276ee16ca927116cd
johnstack <joaovictormtl@gmail.com>
2024-07-28 16:20:30 -0300

    feat:"add verificação da sigla"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/SistemaMedida.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/SistemaMedida.java
@@ -2,6 +2,20 @@ package edu.joao.segundasemana;
 
 public class SistemaMedida {
     public static void main(String [] args){
-        
+        char sigla = 'M';
+
+        switch(sigla){
+            case 'P':
+                System.out.println("PEQUENO");
+                break;
+            case 'M':
+                System.out.println("MÉDIO");
+                break;
+            case 'G':
+                System.out.println("GRANDE");
+                break;
+            default:
+                System.out.println("INDEFINIDO!");
+        }
     }
 }
