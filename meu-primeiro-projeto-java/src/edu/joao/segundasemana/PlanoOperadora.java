9f111a4fe702e2c7154d3bb58931a42a6c26f7b7
johnstack <joaovictormtl@gmail.com>
2024-07-28 16:29:24 -0300

    feat:"add verificar plano"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/PlanoOperadora.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/PlanoOperadora.java
@@ -2,6 +2,18 @@ package edu.joao.segundasemana;
 
 public class PlanoOperadora {
     public static void main(String [] args){
-        
+        char plano = 'B';
+
+        switch(plano){
+            case 'T':
+                System.out.println("5G de Youtube");
+                break;
+            case 'M':
+                System.out.println("Whats e Instagram Grátis");
+                break;
+            case 'B':
+                System.out.println("100 minutos de ligação");
+                break;
+        }
     }
 }
