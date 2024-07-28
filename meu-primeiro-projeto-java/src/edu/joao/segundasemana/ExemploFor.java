13c07ca228206bab0a844a6abbd807f508105cae
johnstack <joaovictormtl@gmail.com>
2024-07-28 17:58:51 -0300

    feat:"add percorrer de maneira abreviada"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploFor.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploFor.java
@@ -6,12 +6,19 @@ public class ExemploFor {
             System.out.println("Contando carneirinhos: "+(i+1));
         }
 
-        System.out.println();
+        System.out.println("\nArray Alunos\n");
 
         String alunos[] = {"João", "Luiz", "Gabriel", "Mônica"};
 
         for(int i = 0; i < alunos.length; i++){
             System.out.println("O aluno do indice "+i+" é: "+alunos[i]);
         }
+
+        System.out.println("\nPercorrendo de maneira abreviada\n");
+
+        for(String aluno : alunos){
+            System.out.println(aluno);
+        }
+
     }
 }
