81f59cc77f5637b8c08db18048afb05c987a478a
johnstack <joaovictormtl@gmail.com>
2024-07-28 16:09:52 -0300

    feat:"add operação ternária para determinar a situação do aluno"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ResultadoEscolar.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ResultadoEscolar.java
@@ -2,7 +2,7 @@ package edu.joao.segundasemana;
 
 public class ResultadoEscolar {
     public static void main(String [] args){
-        int nota = 5;
+        int nota = 8;
 
         if(nota >= 7){
             System.out.println("Aprovado!");
@@ -11,5 +11,8 @@ public class ResultadoEscolar {
         } else{
             System.out.println("Reprovado...");
         }
+
+        String resultado = nota >= 7 ? "Aprovado!" : nota >= 5 && nota < 7 ? "Recuperação!" : "Reprovado...";
+        System.out.println(resultado);
     }
 }
