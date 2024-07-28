ef0f46ffdb64598db80df75669d9b6d9d132e5b4
johnstack <joaovictormtl@gmail.com>
2024-07-28 16:03:15 -0300

    feat:"add verificação da possibilidade de recuperação"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ResultadoEscolar.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ResultadoEscolar.java
@@ -2,10 +2,12 @@ package edu.joao.segundasemana;
 
 public class ResultadoEscolar {
     public static void main(String [] args){
-        int nota = 8;
+        int nota = 5;
 
         if(nota >= 7){
             System.out.println("Aprovado!");
+        } else if(nota >= 5 && nota < 7){
+            System.out.println("Recuperação!");
         } else{
             System.out.println("Reprovado...");
         }
