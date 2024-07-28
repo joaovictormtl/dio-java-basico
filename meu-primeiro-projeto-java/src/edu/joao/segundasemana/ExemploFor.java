52fdcfe40dfeae0e23049012a296f051697bcee5
johnstack <joaovictormtl@gmail.com>
2024-07-28 16:54:41 -0300

    feat:"add estrutura de repetição que faz contagem"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploFor.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploFor.java
@@ -2,6 +2,8 @@ package edu.joao.segundasemana;
 
 public class ExemploFor {
     public static void main(String [] args){
-        
+        for(int i = 0; i < 20; i++){
+            System.out.println("Contando carneirinhos: "+(i+1));
+        }
     }
 }
