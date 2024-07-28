e68cf96dfbe7b3c5d1e87f221170de1e738b7fe5
johnstack <joaovictormtl@gmail.com>
2024-07-28 15:53:51 -0300

    feat:"add melhorias na lógica"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/CaixaEletronico.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/CaixaEletronico.java
@@ -3,11 +3,13 @@ package edu.joao.segundasemana;
 public class CaixaEletronico {
     public static void main(String [] args){
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 27.0;
 
         if(valorSolicitado < saldo){
             saldo -= valorSolicitado;
+            System.out.println("Novo Saldo: "+saldo);
+        } else{
+            System.out.println("Saldo Insuficiente: "+saldo);
         }
-        System.out.println(saldo);
     }
 }
