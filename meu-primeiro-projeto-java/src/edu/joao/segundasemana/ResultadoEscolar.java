c085206bea60e4731b9566262df0427e24c4a244
johnstack <joaovictormtl@gmail.com>
2024-07-28 15:50:24 -0300

    feat:"add controle de fluxo composto conforme a nota"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ResultadoEscolar.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ResultadoEscolar.java
@@ -2,6 +2,12 @@ package edu.joao.segundasemana;
 
 public class ResultadoEscolar {
     public static void main(String [] args){
-        
+        int nota = 8;
+
+        if(nota >= 7){
+            System.out.println("Aprovado!");
+        } else{
+            System.out.println("Reprovado...");
+        }
     }
 }
