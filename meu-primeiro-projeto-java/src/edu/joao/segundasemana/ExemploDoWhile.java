8d1ecef0adef8a82441d73ac3e223df040d9ed2c
johnstack <joaovictormtl@gmail.com>
2024-07-28 18:51:15 -0300

    feat:"add bloco do é executado quantas vezes necessárias"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploDoWhile.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/ExemploDoWhile.java
@@ -1,7 +1,19 @@
 package edu.joao.segundasemana;
 
+import java.util.Random;
+
 public class ExemploDoWhile {
     public static void main(String[] args) {
-        System.out.println("Tudo Certo!");
+        System.out.println("Discando...");
+
+        do{
+            System.out.println("Telefone Tocando...");
+        } while(tocando());
+        System.out.println("Alô!");
+    }
+
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3) == 1;
+        return !atendeu;
     }
 }
