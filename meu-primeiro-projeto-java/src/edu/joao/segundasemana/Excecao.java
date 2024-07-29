7f4eca14c5b667689ab819a670ded3f3321b981b
johnstack <joaovictormtl@gmail.com>
2024-07-29 16:45:50 -0300

    feat:"add receber dados do usuario"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/Excecao.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/Excecao.java
@@ -1,7 +1,30 @@
 package edu.joao.segundasemana;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Excecao {
     public static void main(String [] args){
-        System.out.print("Tudo Certo!");
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite seu nome: ");
+        String nome = input.next();
+
+        System.out.print("Digite seu sobrenome: ");
+        String sobrenome = input.next();
+        
+        System.out.print("Digite a sua idade: ");
+        int idade = input.nextInt();
+
+        System.out.print("Digite a sua altura: ");
+        double altura = input.nextDouble();
+
+        System.out.println();
+
+        System.out.println("Olá, me chamo "+ nome +" "+ sobrenome+".");
+        System.out.println("Tenho "+idade+" anos de idade.");
+        System.out.println("Minha altura é de "+altura+"cm.");
+
+        input.close();
     }
 }
