3e07a8bd89035da9c8afba53b3bedcf10027471b
johnstack <joaovictormtl@gmail.com>
2024-07-29 16:55:35 -0300

    fix:"adicionar tratamento para InputMismatchException"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/Excecao.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/Excecao.java
@@ -1,30 +1,35 @@
 package edu.joao.segundasemana;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class Excecao {
     public static void main(String [] args){
-        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+        try{
+            Scanner input = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.print("Digite seu nome: ");
-        String nome = input.next();
+            System.out.print("Digite seu nome: ");
+            String nome = input.next();
 
-        System.out.print("Digite seu sobrenome: ");
-        String sobrenome = input.next();
-        
-        System.out.print("Digite a sua idade: ");
-        int idade = input.nextInt();
+            System.out.print("Digite seu sobrenome: ");
+            String sobrenome = input.next();
+            
+            System.out.print("Digite a sua idade: ");
+            int idade = input.nextInt();
 
-        System.out.print("Digite a sua altura: ");
-        double altura = input.nextDouble();
+            System.out.print("Digite a sua altura: ");
+            double altura = input.nextDouble();
 
-        System.out.println();
+            System.out.println();
 
-        System.out.println("Olá, me chamo "+ nome +" "+ sobrenome+".");
-        System.out.println("Tenho "+idade+" anos de idade.");
-        System.out.println("Minha altura é de "+altura+"cm.");
+            System.out.println("Olá, me chamo "+ nome +" "+ sobrenome+".");
+            System.out.println("Tenho "+idade+" anos de idade.");
+            System.out.println("Minha altura é de "+altura+"cm.");
 
-        input.close();
+            input.close();
+        } catch(InputMismatchException e){
+            System.err.println("Os campos 'idade' e 'altura' precisam ser numericos!");
+        }
     }
 }
