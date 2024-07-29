e5e75e53dfe813606ebfa3c9f6fe0cd45fb4c55c
johnstack <joaovictormtl@gmail.com>
2024-07-29 17:14:03 -0300

    refactor:"Arrumando a classe"

--- meu-primeiro-projeto-java/src/edu/joao/segundasemana/CepInvalidoException.java
+++ meu-primeiro-projeto-java/src/edu/joao/segundasemana/CepInvalidoException.java
@@ -1,10 +1,5 @@
 package edu.joao.segundasemana;
 
 public class CepInvalidoException extends Exception{
-    static String formatarCep(String cep) throws CepInvalidoException{
-        if(cep.length() != 8){
-            throw new CepInvalidoException();
-        }
-        return "51.250-370";
-    }
+    
 }
