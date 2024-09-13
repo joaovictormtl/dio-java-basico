216f30ba73a9f00f23d3a4fde3ae2ff90b6f8ac4
Joao Melo <joaovictormtl@gmail.com>
2024-09-13 17:13:46 -0300

    fix: corrigir erro de indentação

--- meu-primeiro-projeto-java/src/edu/joao/terceirasemana/ListaTarefas.java
+++ meu-primeiro-projeto-java/src/edu/joao/terceirasemana/ListaTarefas.java
@@ -29,7 +29,7 @@ public class ListaTarefas{
     }
 
     public void obterDescricoesTarefas(){
-       System.out.println(tarefaList);
+        System.out.println(tarefaList);
     }
 
     public static void main(String[] args) {
